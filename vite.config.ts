7ca3f1139d684e06a878c6aae681b6f0f1ec6204
geetharenu <146367541+geetharenu@users.noreply.github.com>
2025-12-11 04:10:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  base: "/",  // VERY IMPORTANT — Without this photos/music gives 404
+
+  build: {
+    outDir: "dist",
+    emptyOutDir: true
+  },
+
   define: {
-    // Some environments might need this to prevent "process is not defined"
-    'process.env': {}
+    "process.env": {}
   }
 });
